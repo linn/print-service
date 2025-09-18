62ef8a4b180a4954bebadd11af92e57c119316b3
Richard Phillips <richard.phillips@linn.co.uk>
2025-09-18 08:19:24 +0100

    fix test and delete duplicate

--- tests/Integration/Integration.Tests/PrintModuleTests/WhenPostingPrintJobWithNoPrinterUri.cs
+++ tests/Integration/Integration.Tests/PrintModuleTests/WhenPostingPrintJobWithNoPrinterUri.cs
@@ -1,13 +1,11 @@
 namespace Linn.PrintService.Integration.Tests.PrintModuleTests
 {
-    using System.Linq;
     using System.Net;
     using System.Net.Http;
     using System.Text;
 
     using FluentAssertions;
 
-    using Linn.PrintService.Printing;
     using Linn.PrintService.Printing.Exceptions;
 
     using NSubstitute;
@@ -33,7 +31,8 @@ namespace Linn.PrintService.Integration.Tests.PrintModuleTests
             this.requestContent.Headers.ContentType =
                 new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
            
-            this.PrintingService.Print(this.printerUri, this.jobName, this.data).Throws(new IppPrintingException("no printer uri"));
+            this.PrintingService.Print(this.printerUri, this.jobName, Arg.Any<byte[]>())
+                .Throws(new IppPrintingException("no printer uri"));
 
             this.Response = this.Client.PostAsync(
                 $"/print-service/print?printerUri={this.printerUri}&jobName={this.jobName}",
