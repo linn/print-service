da5da2b92a8c23463ab32cff682d7f356fd34b02
Ross Stewart <ross.stewart@linn.co.uk>
2025-09-22 15:05:27 +0100

    missing Fi

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -30,6 +30,8 @@ if [ "${TRAVIS_BRANCH}" = "main" ]; then
     PROXY_ROOT=http://app.linn.co.uk
     ENV_SUFFIX=-sys
   fi
+fi
+
 
 # load the secret variables but hide the output from the travis log
 source ./secrets.env > /dev/null 2>&1
